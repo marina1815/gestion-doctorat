7258122d0ff1990c4752fde511cfc8107778da26
hanaaAmira9 <hanaaamira9@gmail.com>
2026-02-21 04:04:30 +0100

    Add user routes for CRUD operations

---
+++ routes/user.route.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import {
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+} from "../controllers/user.controller";
+
+import {
+    createUserSchema,
+    updateUserSchema,
+} from "../dto/user.dto";
+// éventuellement un middleware de RBAC
+// import { requireRole } from "../middlewares/requireRole";
+
+const router = Router();
+
+// Exemple : seulement ADMIN peut créer un user
+// router.post("/", requireRole("ADMIN"), validateBody(createUserSchema), createUser);
+
+router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+export default router;
