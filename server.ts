41469b61ddf977b35bebc05fa49e7a1ac39a9583
hp <hanaaamira9@gmail.com>
2026-02-28 06:13:02 +0100

    update a

--- server.ts
+++ server.ts
@@ -3,6 +3,12 @@ import express, { Request, Response } from "express";
 import path from "path";
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
+import faculteRoutes from "./routes/faculte.routes";
+import departementRoutes from "./routes/departement.routes";
+import concoursRoutes from "./routes/concours.routes";
+import candidatRoutes from "./routes/candidat.routes";
+import specialiteRoutes from "./routes/specialite.routes";
+import membreRoutes from "./routes/membre.routes";
 import { errorHandler, notFoundHandler } from "./middleware/errorHandler";
 import { sanitize } from "./middleware/sanitize";
 import cookieParser from "cookie-parser";
@@ -26,6 +32,12 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/facultes", faculteRoutes);
+app.use("/departements", departementRoutes);
+app.use("/concours", concoursRoutes);
+app.use("/specialites", specialiteRoutes);
+app.use("/candidats", candidatRoutes);
+app.use("/membres",membreRoutes);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
